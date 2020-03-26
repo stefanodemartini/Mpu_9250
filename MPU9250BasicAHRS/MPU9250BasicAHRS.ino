d2b7d5669796a232f58380b887007d3a1b66db65
stefanodemartini <31214217+stefanodemartini@users.noreply.github.com>
2020-03-26 12:28:37 +0100

    Update MPU9250BasicAHRS.ino

--- MPU9250BasicAHRS/MPU9250BasicAHRS.ino
+++ MPU9250BasicAHRS/MPU9250BasicAHRS.ino
@@ -190,16 +190,13 @@ bool newMagData = false;
 volatile bool newData = false;
 int intPin = 14;                                              // These can be changed, 2 and 3 are the Arduinos ext int pins
 int myLed = 13;                                               // Set up pin 13 led for toggling
-
+int16_t MPU9250Data[7];                                       // used to read all 14 bytes at once from the MPU9250 accel/gyro
 int16_t accelCount[3];                                        // Stores the 16-bit signed accelerometer sensor output
 int16_t gyroCount[3];                                         // Stores the 16-bit signed gyro sensor output
 int16_t magCount[3];                                          // Stores the 16-bit signed magnetometer sensor output
-
 float magCalibration[3] = {0, 0, 0}, magbias[3] = {0, 0, 0};  // Factory mag calibration and mag bias
-
 float gyroBias[3] = {0, 0, 0}, accelBias[3] = {0, 0, 0};      // Bias corrections for gyro and accelerometer
 float magBias[3], magScale[3];
-
 int16_t tempCount;                                            // temperature raw count output
 float   temperature;                                          // Stores the real internal chip temperature in degrees Celsius
 float   SelfTest[6];                                          // holds results of gyro and accelerometer self test
@@ -257,13 +254,22 @@ void setup()
     Serial.println("MPU9250 is online...");
 
     MPU9250SelfTest(SelfTest); // Start by performing self test and reporting values
+
     Serial.print("x-axis self test: acceleration trim within: "); Serial.print(SelfTest[0], 1); Serial.println("% of factory value");
     Serial.print("y-axis self test: acceleration trim within: "); Serial.print(SelfTest[1], 1); Serial.println("% of factory value");
     Serial.print("z-axis self test: acceleration trim within: "); Serial.print(SelfTest[2], 1); Serial.println("% of factory value");
     Serial.print("x-axis self test: gyration trim within: "); Serial.print(SelfTest[3], 1); Serial.println("% of factory value");
     Serial.print("y-axis self test: gyration trim within: "); Serial.print(SelfTest[4], 1); Serial.println("% of factory value");
     Serial.print("z-axis self test: gyration trim within: "); Serial.print(SelfTest[5], 1); Serial.println("% of factory value");
+    delay(1000);
 
+    // get sensor resolutions, only need to do this once
+    getAres();
+    getGres();
+    getMres();
+
+
+    Serial.println(" Calibrate gyro and accel");
     accelgyrocalMPU9250(gyroBias, accelBias); // Calibrate gyro and accelerometers, load biases in bias registers
 
     Serial.println("MPU9250 bias");
@@ -282,7 +288,6 @@ void setup()
     delay(2000);
 
     // Get magnetometer calibration from AK8963 ROM
-
     initAK8963(magCalibration); Serial.println("AK8963 initialized for active data mode...."); // Initialize device for active mode read of magnetometer
 
     magcalMPU9250(magBias, magScale);
@@ -309,20 +314,25 @@ void setup()
 
 
 
-void loop() {
+void loop()
+{
   // If intPin goes high, all data registers have new data
-  if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {               // On interrupt, check if data ready interrupt
-    
-    readAccelData(accelCount);                                      // Read the x/y/z adc values
-    getAres();
+  if (newData == true) {                                                // On interrupt, read data
+    newData = false;                                                    // reset newData flag
+
+    readMPU9250Data(MPU9250Data); // INT cleared on any read
+
+    //   if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01) {               // On interrupt, check if data ready interrupt
+
+    //      readAccelData(accelCount);                                     // Read the x/y/z adc values
 
     // Now we'll calculate the accleration value into actual g's
     ax = (float)accelCount[0] * aRes; // - accelBias[0];            // get actual g value, this depends on scale being set
     ay = (float)accelCount[1] * aRes; // - accelBias[1];
     az = (float)accelCount[2] * aRes; // - accelBias[2];
 
-    readGyroData(gyroCount);  // Read the x/y/z adc values
-    getGres();
+    //      readGyroData(gyroCount);  // Read the x/y/z adc values
+
 
     // Calculate the gyro value into actual degrees per second
     gx = (float)gyroCount[0] * gRes;                                // get actual gyro value, this depends on scale being set
@@ -331,7 +341,6 @@ void loop() {
 
     readMagData(magCount);  // Read the x/y/z adc values
 
-    getMres();
     // Calculate the magnetometer values in milliGauss
     // Include factory calibration per data sheet and user environmental corrections
     if (newMagData == true) {
@@ -370,33 +379,25 @@ void loop() {
     if (delt_t > 500) {
 
       if (SerialDebug) {
-        // Print acceleration values in G!
-        //        Serial.print("X-acceleration: "); Serial.print(1000 * ax); Serial.print(" mg ");
-        //        Serial.print("Y-acceleration: "); Serial.print(1000 * ay); Serial.print(" mg ");
-        //        Serial.print("Z-acceleration: "); Serial.print(1000 * az); Serial.println(" mg ");
+        Serial.print("ax = "); Serial.print((int)1000 * ax);
+        Serial.print(" ay = "); Serial.print((int)1000 * ay);
+        Serial.print(" az = "); Serial.print((int)1000 * az); Serial.println(" mg");
+        Serial.print("gx = "); Serial.print( gx, 2);
+        Serial.print(" gy = "); Serial.print( gy, 2);
+        Serial.print(" gz = "); Serial.print( gz, 2); Serial.println(" deg/s");
+        Serial.print("mx = "); Serial.print( (int)mx );
+        Serial.print(" my = "); Serial.print( (int)my );
+        Serial.print(" mz = "); Serial.print( (int)mz ); Serial.println(" mG");
 
-        Serial.print("X-acceleration: "); Serial.print(ax); Serial.print(" G");
-        Serial.print("Y-acceleration: "); Serial.print(ay); Serial.print(" G");
-        Serial.print("Z-acceleration: "); Serial.print(az); Serial.println(" G");
-
-        // Print gyro values in degree/sec
-        Serial.print("X-gyro rate: "); Serial.print(gx, 3); Serial.print(" degrees/sec ");
-        Serial.print("Y-gyro rate: "); Serial.print(gy, 3); Serial.print(" degrees/sec ");
-        Serial.print("Z-gyro rate: "); Serial.print(gz, 3); Serial.println(" degrees/sec");
-
-        // Print mag values in milliGauss
-        Serial.print("X-mag field: "); Serial.print(mx); Serial.print(" mG ");
-        Serial.print("Y-mag field: "); Serial.print(my); Serial.print(" mG ");
-        Serial.print("Z-mag field: "); Serial.print(mz); Serial.println(" mG");
-
-        Serial.print(atan2(mx, my) * 180 / M_PI);
-
-        tempCount = readTempData();  // Read the adc values
-        temperature = ((float) tempCount) / 333.87 + 21.0; // Temperature in degrees Centigrade
-
-        // Print temperature in degrees Centigrade
-        Serial.print("Temperature is ");  Serial.print(temperature, 1);  Serial.println(" degrees C"); // Print T values to tenths of s degree C
+        Serial.print("q0 = "); Serial.print(q[0]);
+        Serial.print(" qx = "); Serial.print(q[1]);
+        Serial.print(" qy = "); Serial.print(q[2]);
+        Serial.print(" qz = "); Serial.println(q[3]);
       }
+      tempCount = readTempData();  // Read the gyro adc values
+      temperature = ((float) tempCount) / 333.87 + 21.0; // Gyro chip temperature in degrees Centigrade
+      // Print temperature in degrees Centigrade
+      Serial.print("Gyro temperature is ");  Serial.print(temperature, 1);  Serial.println(" degrees C"); // Print T values to tenths of s degree C
 
       count = millis();
       digitalWrite(myLed, !digitalRead(myLed));  // toggle led
@@ -548,12 +549,12 @@ void readMPU9250Data(int16_t * destination)
   uint8_t rawData[14];  // x/y/z accel register data stored here
   readBytes(MPU9250_ADDRESS, ACCEL_XOUT_H, 14, &rawData[0]);  // Read the 14 raw data registers into data array
   destination[0] = ((int16_t)rawData[0] << 8) | rawData[1] ;  // Turn the MSB and LSB into a signed 16-bit value
-  destination[1] = ((int16_t)rawData[2] << 8) | rawData[3] ;  
-  destination[2] = ((int16_t)rawData[4] << 8) | rawData[5] ; 
-  destination[3] = ((int16_t)rawData[6] << 8) | rawData[7] ;   
-  destination[4] = ((int16_t)rawData[8] << 8) | rawData[9] ;  
-  destination[5] = ((int16_t)rawData[10] << 8) | rawData[11] ;  
-  destination[6] = ((int16_t)rawData[12] << 8) | rawData[13] ; 
+  destination[1] = ((int16_t)rawData[2] << 8) | rawData[3] ;
+  destination[2] = ((int16_t)rawData[4] << 8) | rawData[5] ;
+  destination[3] = ((int16_t)rawData[6] << 8) | rawData[7] ;
+  destination[4] = ((int16_t)rawData[8] << 8) | rawData[9] ;
+  destination[5] = ((int16_t)rawData[10] << 8) | rawData[11] ;
+  destination[6] = ((int16_t)rawData[12] << 8) | rawData[13] ;
 }
 
 void readAccelData(int16_t * destination) {
